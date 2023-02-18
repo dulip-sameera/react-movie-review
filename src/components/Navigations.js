558a9ac8f029d1a7516124406622101d92e0bed6
Dulip Sameera <dsgitrepo101@gmail.com>
2023-02-18 14:51:40 +0530

    update transition duration

--- src/components/Navigations.js
+++ src/components/Navigations.js
@@ -3,10 +3,10 @@ import React from "react";
 const Navigations = () => {
   return (
     <div className="flex gap-4 text-white text-lg">
-      <div className="p-1 hover:cursor-pointer hover:bg-white hover:text-orange hover:border-white hover:rounded-md transition-all duration-500">
+      <div className="p-1 hover:cursor-pointer hover:bg-white hover:text-orange hover:border-white hover:rounded-md transition-all duration-300">
         Movies
       </div>
-      <div className="p-1 hover:cursor-pointer hover:bg-white hover:text-orange hover:border-white hover:rounded-md transition-all duration-500">
+      <div className="p-1 hover:cursor-pointer hover:bg-white hover:text-orange hover:border-white hover:rounded-md transition-all duration-300">
         TV Series
       </div>
     </div>
