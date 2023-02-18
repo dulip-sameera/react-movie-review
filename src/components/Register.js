95c6f01ddbd0f0b064206fd568dc081a99f6566e
Dulip Sameera <dsgitrepo101@gmail.com>
2023-02-18 14:51:20 +0530

    add registration component

---
+++ src/components/Register.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <div className="flex gap-4 text-white text-lg">
+      <div className="p-1 hover:cursor-pointer hover:bg-white hover:text-orange hover:border-white hover:rounded-md transition-all duration-300">
+        Sign Up
+      </div>
+      <div className="p-1 hover:cursor-pointer hover:bg-white hover:text-orange hover:border-white hover:rounded-md transition-all duration-300">
+        Sing In
+      </div>
+    </div>
+  );
+};
+
+export default Register;
