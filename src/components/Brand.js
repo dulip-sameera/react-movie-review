af2fb3fadecda1ae83d002db3be9243781bca697
Dulip Sameera <dsgitrepo101@gmail.com>
2023-02-18 13:33:02 +0530

    create brand name

---
+++ src/components/Brand.js
@@ -0,0 +1,8 @@
+import React from "react";
+import logo from "../assets/logo.png";
+
+const Brand = () => {
+  return <img src={logo} alt="brand logo" className="w-16 md:w-20 lg:w-25" />;
+};
+
+export default Brand;
