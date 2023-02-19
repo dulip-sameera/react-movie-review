8bae1f201eb91b28ac900b4e2f643a8d0296982b
Dulip Sameera <dsgitrepo101@gmail.com>
2023-02-19 21:27:43 +0530

    add tv api call urls

--- src/utils/URL.js
+++ src/utils/URL.js
@@ -9,3 +9,10 @@ export const upComingMoviesURL = `https://api.themoviedb.org/3/movie/upcoming?ap
 
 // TV SERIES
 export const popularTVSeriesURL = `https://api.themoviedb.org/3/tv/popular?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&page=1`;
+
+export const topRatedTVSeriesURL = `
+https://api.themoviedb.org/3/tv/top_rated?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&page=1`;
+
+export const airingTodayTVSeriesURL = `https://api.themoviedb.org/3/tv/airing_today?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&page=1`;
+
+export const onTheAirTVSeriesURL = `https://api.themoviedb.org/3/tv/on_the_air?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&page=1`;
