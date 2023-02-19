d23d5c4b7d534a5881ab0ea79037e8d9e49bcb20
Dulip Sameera <dsgitrepo101@gmail.com>
2023-02-19 13:18:23 +0530

    create title component

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Title = ({ name }) => {
   return (
-    <div className="border-l-8 border-orange pl-3 text-sm md:text-lg font-medium">
+    <div className="border-l-8 border-orange pl-3 text-base md:text-lg font-medium hover:cursor-default bg-orange bg-opacity-5">
       {name}
     </div>
   );
