53ecc698eed17b093273864406b5f53ff1768311
Dulip Sameera <dsgitrepo101@gmail.com>
2023-02-19 21:58:26 +0530

    add routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./layout/NavBar";
 import Home from "./pages/Home";
+import Movie from "./pages/Movie";
+import Show from "./pages/Show";
+import TVSeries from "./pages/TVSeries";
 
 function App() {
   return (
-    <div className="bg-gray">
-      <NavBar />
-      <Home />
-    </div>
+    <BrowserRouter>
+      <div className="bg-gray">
+        <NavBar />
+
+        <Routes>
+          {["/", "/home"].map((path, index) => (
+            <Route path={path} element={<Home />} key={index} />
+          ))}
+          <Route path="/movies" element={<Movie />} />
+          <Route path="/tvseries" element={<TVSeries />} />
+          <Route path="/movies/:id" element={<Show />} />
+          <Route path="/tvseries/:id" element={<Show />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
