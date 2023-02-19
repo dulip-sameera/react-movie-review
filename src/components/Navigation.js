4c0a6d292731a5b4ded2e0df8dd52e81070c9b35
Dulip Sameera <dsgitrepo101@gmail.com>
2023-02-19 08:45:42 +0530

    make hidden on sm screens

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navigation = () => {
   return (
-    <div className="flex gap-4 text-white text-lg">
+    <div className="sm:flex gap-4 text-white text-lg hidden ">
       <div className="p-1 hover:cursor-pointer hover:bg-white hover:text-orange hover:border-white hover:rounded-md transition-all duration-300">
         Movies
       </div>
--- src/components/Register.js
+++ src/components/Register.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Register = () => {
   return (
-    <div className="flex gap-4 text-white text-lg">
+    <div className="hidden sm:flex gap-4 text-white text-lg">
       <div className="p-1 hover:cursor-pointer hover:bg-white hover:text-orange hover:border-white hover:rounded-md transition-all duration-300">
         Sign Up
       </div>
