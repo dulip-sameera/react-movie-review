69ff37b96c93130eb52ec7e608b00800769f8162
Dulip Sameera <dsgitrepo101@gmail.com>
2023-02-19 21:21:14 +0530

    create movie page

---
+++ src/pages/Movie.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Section from "../components/Section";
+import { useFetch } from "../hook/useFetch";
+import { posterBaseURL } from "../utils/Data";
+import { extractMovieData } from "../utils/extractMovieData";
+import {
+  nowPlayingMoviesURL,
+  popularMoviesURL,
+  topRatedMoviesURL,
+  upComingMoviesURL,
+} from "../utils/URL";
+
+const Movie = () => {
+  //   Fetch popular movies
+  const popularMovies = useFetch(popularMoviesURL);
+  const popularMovieList = extractMovieData(popularMovies, posterBaseURL);
+  //   ---------------------------------
+
+  //   Fetch Top Rated Movies
+  const topRatedMovies = useFetch(topRatedMoviesURL);
+  const topRatedMovieList = extractMovieData(topRatedMovies, posterBaseURL);
+  //   ---------------------------------
+
+  //   Fetch Now Playing Movies
+  const nowPlayingMovies = useFetch(nowPlayingMoviesURL);
+  const nowPlayingMovieList = extractMovieData(nowPlayingMovies, posterBaseURL);
+  //   ---------------------------------
+
+  //   Fetch Up Coming Movies
+  const upComingMovies = useFetch(upComingMoviesURL);
+  const upComingMovieList = extractMovieData(upComingMovies, posterBaseURL);
+
+  return (
+    <div className="container p-10 bg-lightGray min-h-screen">
+      {nowPlayingMovies.loading && <div>Loading...</div>}
+      {nowPlayingMovies.data && (
+        <Section title={"NOW PLAYING MOVIES"} data={nowPlayingMovieList} />
+      )}
+      {upComingMovies.loading && <div>Loading...</div>}
+      {upComingMovies.data && (
+        <Section title={"UP COMING MOVIES"} data={upComingMovieList} />
+      )}
+      {topRatedMovies.loading && <div>Loading...</div>}
+      {topRatedMovies.data && (
+        <Section title={"TOP RATED MOVIES"} data={topRatedMovieList} />
+      )}
+      {popularMovies.loading && <div>Loading...</div>}
+      {popularMovies.data && (
+        <Section title={"POPULAR MOVIES"} data={popularMovieList} />
+      )}
+    </div>
+  );
+};
+
+export default Movie;
