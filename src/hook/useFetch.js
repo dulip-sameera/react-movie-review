44a365fdb8c34e5d43b2a2a033eb0edd7007e509
Dulip Sameera <dsgitrepo101@gmail.com>
2023-02-19 21:21:02 +0530

    create custom hook to fetch data

---
+++ src/hook/useFetch.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export function useFetch(url) {
+  const [status, setStatus] = useState();
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    (async function () {
+      try {
+        setLoading(true);
+        const response = await axios.get(url);
+        setStatus(response.status);
+        setData(response.data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, [url]);
+
+  return { data, error, loading, status };
+}
