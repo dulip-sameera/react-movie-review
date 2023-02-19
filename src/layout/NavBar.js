5723d2367572b6423827fd8273aaeff7f1fc66ad
Dulip Sameera <dsgitrepo101@gmail.com>
2023-02-19 21:20:45 +0530

    add home route link

--- src/layout/NavBar.js
+++ src/layout/NavBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Brand from "../components/Brand";
 import Navigation from "../components/Navigation";
 import Register from "../components/Register";
@@ -8,7 +9,9 @@ const NavBar = () => {
   return (
     <div className="bg-orange p-4">
       <div className="container flex justify-between items-center">
-        <Brand />
+        <Link to={"/"}>
+          <Brand />
+        </Link>
         <SearchBar />
         <Navigation />
         <Register />
