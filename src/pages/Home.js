ed4363fc325043a68898128a0af8bc3cf2b38c73
Dulip Sameera <dsgitrepo101@gmail.com>
2023-02-19 21:20:06 +0530

    remove repetitive code blocks

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,59 +1,31 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
 import Section from "../components/Section";
+import { useFetch } from "../hook/useFetch";
+import { posterBaseURL } from "../utils/Data";
+import { extractMovieData } from "../utils/extractMovieData";
+import { extractTVData } from "../utils/extractTVData";
+import { popularMoviesURL, popularTVSeriesURL } from "../utils/URL";
 
 const Home = () => {
-  const [popularMovies, setPopularMovies] = useState([]);
-  const [popularTVSeries, setPopularTVSeries] = useState([]);
+  //   Fetch popular movies
+  const popularMovies = useFetch(popularMoviesURL);
+  const popularMovieList = extractMovieData(popularMovies, posterBaseURL);
 
-  const popularMoviesURL = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&page=1`;
-
-  const popularTVSeriesURL = `https://api.themoviedb.org/3/tv/popular?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&page=1`;
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const fetchData = async () => {
-    // image url for poster
-    const imageBaseURL = "http://image.tmdb.org/t/p/";
-    const posterSize = "w500";
-    const posterBaseURL = `${imageBaseURL}${posterSize}`;
-
-    // fetch popular movies
-    const movieResult = await axios.get(popularMoviesURL);
-    // stop working if request didn't send the data
-    if (movieResult.status !== 200) return;
-    const movieList = movieResult.data.results;
-    const requiredMovieDataList = movieList.map((item) => ({
-      id: item.id,
-      poster: `${posterBaseURL}${item.poster_path}`,
-      title: item.title,
-      vote: item.vote_average,
-    }));
-    setPopularMovies(requiredMovieDataList);
-
-    // fetch popular tv series
-    const tvResult = await axios.get(popularTVSeriesURL);
-    // stop working if request didn't send the data
-    if (tvResult.status !== 200) return;
-    const tvList = tvResult.data.results;
-    const requiredTvDataList = tvList.map((item) => ({
-      id: item.id,
-      poster: `${posterBaseURL}${item.poster_path}`,
-      title: item.name,
-      vote: item.vote_average,
-    }));
-    setPopularTVSeries(requiredTvDataList);
-  };
+  // Fetch Popular TV Series
+  const popularTVSeries = useFetch(popularTVSeriesURL);
+  const popularTVSeriesList = extractTVData(popularTVSeries, posterBaseURL);
 
   return (
-    <>
-      <div className="container p-10 bg-lightGray min-h-screen">
-        <Section title={"POPULAR MOVIES"} data={popularMovies} />
-        <Section title={"POPULAR TV SERIES"} data={popularTVSeries} />
-      </div>
-    </>
+    <div className="container p-10 bg-lightGray min-h-screen">
+      {popularMovies.loading && <div>Loading...</div>}
+      {popularMovies.data && (
+        <Section title={"POPULAR MOVIES"} data={popularMovieList} />
+      )}
+
+      {popularTVSeries.loading && <div>Loading...</div>}
+      {popularTVSeries.data && (
+        <Section title={"POPULAR TV SERIES"} data={popularTVSeriesList} />
+      )}
+    </div>
   );
 };
 
