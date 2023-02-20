d2e92e38a7964aca583d44f62b39fe2fe607d561
Dulip Sameera <dsgitrepo101@gmail.com>
2023-02-20 13:42:18 +0530

    update to show custom overview text if api hasn't one

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,15 +1,18 @@
 import React from "react";
 import Title from "./Title";
 
-const Description = ({ poster, overview, vote }) => {
+const Description = ({ poster, overview, vote, type }) => {
   return (
     <div className="mt-4 md:flex md:gap-5">
       <div className="flex justify-center">
-        <img src={poster} alt="" className="" />
+        <img src={poster} alt="" className="max-w-xs" />
       </div>
       <div className=" mt-4">
         <Title name={"OVERVIEW"} />
-        <div className="text-justify">{overview}</div>
+        <div className="text-justify">
+          {overview !== "" ? overview : `This is a ${type} :)`}
+        </div>
+
         <div className="text-center mt-3 md:text-left md:mt-6">
           <div className="font-medium">
             SCORE : {Math.round(vote * 10) / 10}/10
