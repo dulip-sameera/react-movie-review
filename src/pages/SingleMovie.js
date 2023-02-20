7d7d528c4c4d4789ed4b1b5914f7a28f7c30afdc
Dulip Sameera <dsgitrepo101@gmail.com>
2023-02-20 13:18:32 +0530

    create single movie page

---
+++ src/pages/SingleMovie.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import Description from "../components/Description";
+import NoTrailer from "../components/NoTrailer";
+import Player from "../components/Player";
+import Title from "../components/Title";
+import { useFetch } from "../hook/useFetch";
+import { posterBase342URL, youtubeBaseURL } from "../utils/Data";
+import extractMovieData from "../utils/extractMovieData";
+
+const SingleMovie = () => {
+  const { sid } = useParams();
+
+  const url = `https://api.themoviedb.org/3/movie/${sid}?language=en-US&api_key=${process.env.REACT_APP_TMDB_API_KEY}&append_to_response=videos`;
+
+  const movie = useFetch(url);
+
+  let data = extractMovieData(movie, posterBase342URL, youtubeBaseURL);
+
+  return (
+    <div className="container p-10 bg-lightGray min-h-screen">
+      {movie.loading && <div>Loading...</div>}
+      {movie.data && <Title name={data.title} />}
+      {movie.data && data.trailer ? (
+        <Player url={data.trailer} />
+      ) : (
+        <NoTrailer />
+      )}
+      {movie.data && (
+        <Description
+          poster={data.poster}
+          overview={data.overview}
+          vote={data.vote}
+        />
+      )}
+    </div>
+  );
+};
+
+export default SingleMovie;
