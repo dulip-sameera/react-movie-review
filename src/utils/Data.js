0a40e9aaf75fecf39be50b8aeb0d2efd11d72681
Dulip Sameera <dsgitrepo101@gmail.com>
2023-02-20 13:17:48 +0530

    add three poster sizes

--- src/utils/Data.js
+++ src/utils/Data.js
@@ -1,6 +1,10 @@
 // image url for poster
 const imageBaseURL = "http://image.tmdb.org/t/p/";
-const posterSize = "w500";
-export const posterBaseURL = `${imageBaseURL}${posterSize}`;
+const posterSize500 = "w500";
+const posterSize342 = "w342";
+const posterSize185 = "w185";
+export const posterBaseURL = `${imageBaseURL}${posterSize500}`;
+export const posterBase342URL = `${imageBaseURL}${posterSize342}`;
+export const posterBase185URL = `${imageBaseURL}${posterSize185}`;
 
 export const youtubeBaseURL = "https://www.youtube.com/watch?v=";
