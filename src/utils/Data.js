9759fe6b0e731b228e9c9da2442bba1933931161
Dulip Sameera <dsgitrepo101@gmail.com>
2023-02-20 11:11:13 +0530

    add youtube base url

--- src/utils/Data.js
+++ src/utils/Data.js
@@ -2,3 +2,5 @@
 const imageBaseURL = "http://image.tmdb.org/t/p/";
 const posterSize = "w500";
 export const posterBaseURL = `${imageBaseURL}${posterSize}`;
+
+export const youtubeBaseURL = "https://www.youtube.com/watch?v=";
