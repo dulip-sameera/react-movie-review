42b187433dc382e10cb3a557efbe890529cd0c66
Dulip Sameera <dsgitrepo101@gmail.com>
2023-02-20 12:03:48 +0530

    create a function to extract single movie data

--- src/utils/extractMovieData.js
+++ src/utils/extractMovieData.js
@@ -1,18 +1,38 @@
-export function extractMovieData(response, posterBaseURL) {
+export default function extractMovieData(
+  response,
+  posterBaseURL,
+  youtubeBaseURL
+) {
   if (response.error) console.log(response.error);
+  let data = {
+    id: "",
+    overview: "",
+    poster: "",
+    title: "",
+    vote: "",
+    trailer: "",
+  };
 
-  let dataList = [];
+  if (
+    response.data &&
+    ("results" in response.data.videos) &
+      (response.data.videos.results !== undefined)
+  ) {
+    for (const item of response.data.videos.results) {
+      if (item.type === "Trailer") {
+        data = {
+          id: response.data.id,
+          overview: response.data.overview,
+          poster: `${posterBaseURL}${response.data.poster_path}`,
+          title: response.data.title,
+          vote: response.data.vote_average,
+          trailer: `${youtubeBaseURL}${item.key}`,
+        };
+        break;
+      }
+    }
+  }
 
-  if (response.data) dataList = response.data.results;
-
-  const data = dataList.map((item) => ({
-    id: item.id,
-    poster: `${posterBaseURL}${item.poster_path}`,
-    title: item.title,
-    vote: item.vote_average,
-    type: "movies",
-    overview: item.overview,
-  }));
-
+  //   console.log(data);
   return data;
 }
