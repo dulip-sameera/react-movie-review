e1783bdd26a100b4ab33ea589e67c39fa970b346
Dulip Sameera <dsgitrepo101@gmail.com>
2023-02-22 07:20:19 +0530

    remove second btn

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,11 +3,8 @@ import React from "react";
 const Register = () => {
   return (
     <div className="hidden sm:flex gap-4 text-white text-lg">
-      <div className="p-1 hover:cursor-pointer hover:bg-white hover:text-orange hover:border-white hover:rounded-md transition-all duration-300">
-        Sign Up
-      </div>
-      <div className="p-1 hover:cursor-pointer hover:bg-white hover:text-orange hover:border-white hover:rounded-md transition-all duration-300">
-        Sing In
+      <div className="py-1 px-2 hover:cursor-pointer hover:bg-white hover:text-orange hover:border-white hover:rounded-md transition-all duration-300">
+        LogIn
       </div>
     </div>
   );
