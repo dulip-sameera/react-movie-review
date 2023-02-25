05077f89065c8f7f56be1819427d8f2b4253733d
Dulip Sameera <dsgitrepo101@gmail.com>
2023-02-25 15:37:46 +0530

    create addreview page

---
+++ src/pages/AddReview.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddReview = () => {
+  return (
+    <div className="container flex justify-center items-center h-[500px]">
+      <div>AddReview</div>
+    </div>
+  );
+};
+
+export default AddReview;
