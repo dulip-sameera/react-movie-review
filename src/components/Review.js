074b74fe72f50b071e9eb0f098c2f0d3ad3e0fdc
Dulip Sameera <dsgitrepo101@gmail.com>
2023-02-25 15:37:27 +0530

    convert btns to links

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import fetchReviewData from "../firebase/fetchReviewData";
 import { userSelector } from "../store/user.slice";
 import ReviewItem from "./ReviewItem";
@@ -26,9 +27,12 @@ const Review = ({ movieId }) => {
         <div className="border-l-8 border-orange pl-3 text-base md:text-lg font-medium hover:cursor-default bg-orange bg-opacity-5 flex justify-between items-center p-2 mt-4">
           <div>Review</div>
           {!("userHasReview" in data[0]) && (
-            <button className="bg-orange text-white py-1 px-2 rounded-lg">
+            <Link
+              to={`/addreview/${movieId}`}
+              className="bg-orange text-white py-1 px-2 rounded-lg"
+            >
               Add Review
-            </button>
+            </Link>
           )}
         </div>
         {data &&
@@ -44,9 +48,12 @@ const Review = ({ movieId }) => {
   } else if (loading) {
     return (
       <div className="mt-4 flex justify-center">
-        <button className="bg-orange text-white py-2 px-4 text-lg rounded-lg">
+        <Link
+          to={`/addreview/${movieId}`}
+          className="bg-orange text-white py-2 px-4 text-lg rounded-lg"
+        >
           Add Review
-        </button>
+        </Link>
       </div>
     );
   }
