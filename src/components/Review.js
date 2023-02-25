6d69ae99d56d3385ae1a393bc2648291dd882c8b
Dulip Sameera <dsgitrepo101@gmail.com>
2023-02-25 15:11:13 +0530

    add expression to decide wether to show the Add Review btn or not

--- src/components/Review.js
+++ src/components/Review.js
@@ -25,9 +25,11 @@ const Review = ({ movieId }) => {
         {/* title */}
         <div className="border-l-8 border-orange pl-3 text-base md:text-lg font-medium hover:cursor-default bg-orange bg-opacity-5 flex justify-between items-center p-2 mt-4">
           <div>Review</div>
-          <button className="bg-orange text-white py-1 px-2 rounded-lg">
-            Add Review
-          </button>
+          {!("userHasReview" in data[0]) && (
+            <button className="bg-orange text-white py-1 px-2 rounded-lg">
+              Add Review
+            </button>
+          )}
         </div>
         {data &&
           data.map((item, index) => (
