cbfcb4ca779164515cec54b944f160fd8cc381a2
Dulip Sameera <dsgitrepo101@gmail.com>
2023-02-25 15:36:49 +0530

    add addreview router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./layout/NavBar";
+import AddReview from "./pages/AddReview";
 import Home from "./pages/Home";
 import Movie from "./pages/Movie";
 import SingleMovie from "./pages/SingleMovie";
@@ -9,7 +10,7 @@ import TVSeries from "./pages/TVSeries";
 function App() {
   return (
     <BrowserRouter>
-      <div className="bg-gray">
+      <div className="bg-gray min-h-screen">
         <NavBar />
 
         <Routes>
@@ -20,6 +21,7 @@ function App() {
           <Route path="/tvseries" element={<TVSeries />} />
           <Route path="/movies/:sid" element={<SingleMovie />} />
           <Route path="/tvseries/:sid" element={<SingleTVSeries />} />
+          <Route path="/addreview/:sid" element={<AddReview />} />
         </Routes>
       </div>
     </BrowserRouter>
