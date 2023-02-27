23c3ac34527dc88884802c424ad9026da4165a1a
Dulip Sameera <dsgitrepo101@gmail.com>
2023-02-27 07:56:57 +0530

    create a new loading component

---
+++ src/utils/Loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="container min-h-[500px] flex justify-center items-center">
+      <div className="text-3xl font-bold">Loading...</div>
+    </div>
+  );
+};
+
+export default Loading;
